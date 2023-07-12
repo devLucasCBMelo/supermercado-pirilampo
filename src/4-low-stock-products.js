01580d692db0cfbc31ebaf26f34f1b92a95c716d
Lucas Costa <englucascbmelo@gmail.com>
2023-07-12 10:41:12 -0300

    colocando no plural

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -4,8 +4,12 @@ const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
   let baixoEstoque = [];
   for (index = 0; index < stockProducts.length; index += 1) {
-    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
-      baixoEstoque.push(`${stockProducts[index].productName} : ${stockProducts[index].quantityInStock} unidade`);
+    if (stockProducts[index].quantityInStock > 1 && stockProducts[index].quantityInStock <= 10) {
+
+      baixoEstoque.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`);
+
+    } else if (stockProducts[index].quantityInStock === 1) {
+      baixoEstoque.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidade`);
     }
   }
   return baixoEstoque;
