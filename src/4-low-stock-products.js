b062e2018023381b2e144353af03ece1179d9fde
Lucas Costa <englucascbmelo@gmail.com>
2023-07-12 10:31:55 -0300

    está correto, mas falta por no plural

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let baixoEstoque = [];
+  for (index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      baixoEstoque.push(`${stockProducts[index].productName} : ${stockProducts[index].quantityInStock} unidade`);
+    }
+  }
+  return baixoEstoque;
 };
+
+getLowStockProducts(stockProducts);
 
 module.exports = { getLowStockProducts };
