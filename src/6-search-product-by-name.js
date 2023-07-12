7b8246801b9a1ffb04957a2258dfc83fac0523f9
Lucas Costa <englucascbmelo@gmail.com>
2023-07-12 11:48:54 -0300

    requisito 6

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
+const searchProductByName = (name) => {
   // Desenvolva seu código dentro dessa função...
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].productName === name) {
+      return {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+      };
+    }
+  }
+
+  return null;
 };
 
+searchProductByName(stockProducts);
 module.exports = { searchProductByName };
