54453f462bf3f2c2474cbf7c7026415a38691ebc
Lucas Costa <englucascbmelo@gmail.com>
2023-07-12 10:53:59 -0300

    requisito 5 - o mais ligeiro

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let totalEstoque = 0;
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    totalEstoque += stockProducts[index].quantityInStock;
+  }
+  return totalEstoque;
 };
+
+getProductsAmount(stockProducts);
 
 module.exports = { getProductsAmount };
