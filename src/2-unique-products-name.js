47b3196d51938f501bba8b5851a47d55639b1806
Lucas Costa <englucascbmelo@gmail.com>
2023-07-12 09:39:09 -0300

    segundo requisito

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,14 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  let nomes = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    nomes.push(stockProducts[index].productName);
+  }
+
+  return nomes;
 };
+
+console.log(getUniqueProductsName(stockProducts));
 
 module.exports = { getUniqueProductsName };
